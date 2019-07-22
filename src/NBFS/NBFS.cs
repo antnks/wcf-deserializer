039688f8c6657732ea261041b67e303935a57567
antnks <antnks@users.noreply.github.com>
2019-07-22 14:23:18 +0300

    Re-applied fix https://github.com/antnks/WCF-Binary-SOAP-Plug-In/commit/7462654f06971a97960d3e12e1a3bd171a3ca01d

--- src/NBFS/NBFS.cs
+++ src/NBFS/NBFS.cs
@@ -217,6 +217,7 @@ public static class WcfDictionaryBuilder
         dict.Add("Transforms");
         dict.Add("Transform");
         dict.Add("DigestMethod");
+        dict.Add("DigestValue");
         dict.Add("Address");
         dict.Add("ReplyTo");
         dict.Add("SequenceAcknowledgement");
